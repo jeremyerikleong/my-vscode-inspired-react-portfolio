ef73450ea85781bbb80d82716346ac9a2fe0bc85
Jeremy <jeremythegreat95@gmail.com>
2025-10-24 00:35:00 +0800

    refactored the sidebar component and added active route detection

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,70 +1,50 @@
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { VscFiles, VscBriefcase, VscCode, VscGithubAlt, VscMail, VscAccount, VscSettings } from 'react-icons/vsc';
+import type { IconType } from 'react-icons';
 
 export default function SideBar() {
     const ICON_SIZE = 24;
     const ICON_DEFAULT_COLORS = '#a7a7a7';
     const ICON_ACTIVE_COLORS = '#fff';
+    const pathname = usePathname();
+
+    const topLinks = [
+        { id: '1', href: '/', icon: VscFiles },
+        { id: '2', href: '/experience', icon: VscBriefcase },
+        { id: '3', href: '/projects', icon: VscCode },
+        { id: '4', href: '/github', icon: VscGithubAlt },
+        { id: '5', href: '/contact', icon: VscMail },
+    ];
+
+    const bottomLinks = [
+        { id: '6', href: '/about', icon: VscAccount },
+        { id: '7', href: '/settings', icon: VscSettings },
+    ];
+
+    function renderButton(id: string, href: string, Icon: IconType) {
+        const isActive = pathname === href;
+
+        return (
+            <li
+                key={`buttonLink${id}`}
+                className={`border-l-2 ${isActive ? 'border-white' : 'border-transparent'}`}>
+                <Link href={href}
+                    className="flex items-center px-3 py-4 cursor-pointer">
+                    <Icon size={ICON_SIZE} color={isActive ? ICON_ACTIVE_COLORS : ICON_DEFAULT_COLORS} />
+                </Link>
+            </li>
+        )
+    }
 
     return (
         <aside className="flex flex-col items-center justify-between h-[calc(100vh-30px-30px)] w-fit py-4 bg-gray-800">
             <div>
-                <li className="border-l-2 border-transparent">
-                    <Link href="/"
-                        className="flex items-center px-3 py-4 cursor-pointer">
-                        <VscFiles size={ICON_SIZE} color={ICON_DEFAULT_COLORS} />
-                    </Link>
-                </li>
-
-                <li className="border-l-2 border-transparent">
-                    <Link
-                        href="./experience"
-                        className="flex items-center px-3 py-4 cursor-pointer">
-                        <VscBriefcase size={ICON_SIZE} color={ICON_DEFAULT_COLORS} />
-                    </Link>
-                </li>
-
-                <li className="border-l-2 border-transparent">
-                    <Link
-                        href="./projects"
-                        className="flex items-center px-3 py-4 cursor-pointer">
-                        <VscCode size={ICON_SIZE} color={ICON_DEFAULT_COLORS} />
-                    </Link>
-                </li>
-
-                <li className="border-l-2">
-                    <Link
-                        href="./github"
-                        className="flex items-center px-3 py-4 cursor-pointer">
-                        <VscGithubAlt size={ICON_SIZE} color={ICON_ACTIVE_COLORS} />
-                    </Link>
-                </li>
-
-                <li className="border-l-2 border-transparent">
-                    <Link
-                        href="./contact"
-                        className="flex items-center px-3 py-4 cursor-pointer">
-                        <VscMail size={ICON_SIZE} color={ICON_DEFAULT_COLORS} />
-                    </Link>
-                </li>
+                {topLinks.map((link) => renderButton(link.id, link.href, link.icon))}
             </div>
 
             <div>
-                <li className="border-l-2 border-transparent">
-                    <Link
-                        href="./about"
-                        className="flex items-center px-3 py-4 cursor-pointer">
-                        <VscAccount size={ICON_SIZE} color={ICON_DEFAULT_COLORS} />
-                    </Link>
-                </li>
-
-                <li className="border-l-2 border-transparent">
-                    <Link
-                        href="./settings"
-                        className="flex items-center px-3 py-4 cursor-pointer">
-                        <VscSettings size={ICON_SIZE} color={ICON_DEFAULT_COLORS} />
-                    </Link>
-                </li>
+                {bottomLinks.map((link) => renderButton(link.id, link.href, link.icon))}
             </div>
         </aside>
     )
