fa144ad2f2489233641e15d0bb1a3ec117444126
Jeremy <jeremythegreat95@gmail.com>
2025-10-25 23:58:54 +0800

    set the initial primary sidebar state based on device detection

--- app/components/ClientLayout.tsx
+++ app/components/ClientLayout.tsx
@@ -8,9 +8,10 @@ import PrimarySideBar from './PrimarySideBar';
 import SecondarySideBar from './SecondarySideBar';
 import Panel from './Panel';
 import Footer from './Footer';
+import { isMobile } from 'react-device-detect';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
-    const [showPrimarySideBar, setShowPrimarySideBar] = useState(true);
+    const [showPrimarySideBar, setShowPrimarySideBar] = useState(() => !isMobile);
     const [showSecondarySideBar, setShowSecondarySideBar] = useState(false);
     const [showPanel, setShowPanel] = useState(false);
 
